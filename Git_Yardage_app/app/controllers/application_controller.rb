19e8304fca61572160465d8620ba08ce5f062a36
bgordon8 <gordonbrian16@gmail.com>
2020-10-01 15:29:43 -0600

    Adds Authenticate action, successful token response

--- Git_Yardage_app/app/controllers/application_controller.rb
+++ Git_Yardage_app/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate 
+    
+    def authenticate
+        auth_header = request.headers["Authorization"]
+
+        if !auth_header
+            render json: {message: "No token"}, status: :forbidden
+        else
+            token = auth_header.split(" ")[1]
+            secret = "hello now there guy"
+            decoded_token = JWT.decode token, secret
+            payload = decoded_token.first
+            user_id = payload["user_id"]
+            @user = User.find(user_id)
+
+
+            render json: {message: "success"}, status: :ok
+        end
+    end
 end
--- Git_Yardage_app/app/controllers/users_controller.rb
+++ Git_Yardage_app/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :authenticate, only: [:create]
 
     def index
         @users = User.all
