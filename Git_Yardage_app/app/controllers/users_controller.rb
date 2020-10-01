05a06a508f478aeb4936cb0f8481347bdc78dd85
bgordon8 <gordonbrian16@gmail.com>
2020-10-01 12:13:29 -0600

    Adds User index, create actions adds bcrypt gem and bundle installs

--- Git_Yardage_app/app/controllers/users_controller.rb
+++ Git_Yardage_app/app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+
+    def create
+        @user = User.create(
+            username: params[:username],
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            email: params[:email],
+            password: params[:password],
+            handicap: params[:handicap]
+        )
+
+        render json: @user
+    end
 end
+
