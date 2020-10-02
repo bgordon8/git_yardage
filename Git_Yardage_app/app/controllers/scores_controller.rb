c9a88df25375ce865d968dd370c7837a9f4375b6
bgordon8 <gordonbrian16@gmail.com>
2020-10-01 22:55:51 -0600

    [WIP] app is data complete :construction:

--- Git_Yardage_app/app/controllers/scores_controller.rb
+++ Git_Yardage_app/app/controllers/scores_controller.rb
@@ -1,2 +1,13 @@
 class ScoresController < ApplicationController
+    def index
+        @scores = Score.all
+
+        render json: @scores, include: [:hole]
+    end
+
+    def show
+        @score = Score.find_by(params[:id])
+    
+        render json: @score, include: [:hole]
+    end
 end
