62d6e0fdb7b666632e3890755ceb17172d845b70
bgordon8 <gordonbrian16@gmail.com>
2020-10-01 20:15:31 -0600

    Adds completed auth register, adds register action, returns token

--- Git_Yardage_app/app/controllers/authentication_controller.rb
+++ Git_Yardage_app/app/controllers/authentication_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticationController < ApplicationController
-    skip_before_action :authenticate, only: [:login]
+    skip_before_action :authenticate, only: [:login, :register]
     
     def login
         @user = User.find_by email: params[:email]
@@ -19,4 +19,29 @@ class AuthenticationController < ApplicationController
         end
 
     end
+
+    
+    def register
+        @user = User.find_by email: params[:email]
+
+        if @user
+            render json: { message: "email taken" }, status: :ok
+        else
+            @user = User.create(
+                username: params[:username],
+                first_name: params[:first_name],
+                last_name: params[:last_name],
+                email: params[:email],
+                password: params[:password],
+                handicap: params[:handicap]
+            )
+
+            payload = { user_id: @user.id }
+            secret = Rails.application.secrets.secret_key_base
+            token = JWT.encode(payload, secret)
+
+            render json: { token: token }, status: :ok
+        end
+    end
+
 end
